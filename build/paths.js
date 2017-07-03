9fc753ea1f8c1de54ecb15eff796b82e6cafa982
Vegar ringdal <vegar.ringdal@gmail.com>
2017-07-03 12:51:25 +0200

    chore(build) add sourcemaps

--- build/paths.js
+++ build/paths.js
@@ -1,11 +1,13 @@
 var srcRoot = 'src/';
 var distRoot = 'dist/';
+var distDevRoot = 'distTemp/';
 
 
 module.exports = {
   root: srcRoot,
   source: srcRoot + '**/*.ts',
   output: distRoot,
+  outputDev: distDevRoot,
   dtsSrc: [
     './typings/**/*.d.ts',
     './custom_typings/**/*.d.ts'
