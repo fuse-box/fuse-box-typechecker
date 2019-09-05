265b3a48ec08f341b94f22cde30e071954489b37
Vegar RIngdal <vegar.ringdal@gmail.com>
2019-09-05 18:46:55 +0200

    chore: print without inspect test

---
+++ scripts/run-test-007 - print without inspect.ts
@@ -0,0 +1,27 @@
+import { TypeHelperClass } from '../src/index';
+import { ITypeCheckerOptions } from '../src/interfaces';
+
+const checker: TypeHelperClass = require('../src/index').TypeChecker(<ITypeCheckerOptions>{
+    basePath: './test',
+    name: 'checkerSync',
+    tsConfigOverride: {
+        compilerOptions: {
+            rootDir: `./test`,
+            baseUrl:`./test`,
+            target: 'es2015',
+            module: 'commonjs',
+            lib: ['es2017', 'dom'],
+            emitDecoratorMetadata: true,
+            sourceMap: true,
+            declaration: true,
+            importHelpers: true,
+            experimentalDecorators: true
+        }
+    },
+    print_summary: true,
+    print_runtime: true
+});
+
+
+checker.printOnly(<any>{});
+
