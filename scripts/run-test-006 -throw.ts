008b49e8d97ea0d7e5ab81d3137a956cb355f090
Vegar RIngdal <vegar.ringdal@gmail.com>
2019-09-05 18:07:22 +0200

    chore: throw print test

---
+++ scripts/run-test-006 -throw.ts
@@ -0,0 +1,24 @@
+import { TypeHelperClass } from '../src/index';
+import { ITypeCheckerOptions } from '../src/interfaces';
+
+const checker: TypeHelperClass = require('../src/index').TypeChecker(<ITypeCheckerOptions>{
+    basePath: './test',
+    name: 'checkerSync',
+    throwOnSemantic:true,
+    tsConfigOverride: {
+        compilerOptions: {
+            rootDir: `./test`,
+            baseUrl:`./test`,
+            target: 'es2015',
+            module: 'commonjs',
+            lib: ['es2017', 'dom'],
+            emitDecoratorMetadata: true,
+            sourceMap: true,
+            declaration: true,
+            importHelpers: true,
+            experimentalDecorators: true
+        }
+    }
+});
+
+checker.inspectAndPrint();
